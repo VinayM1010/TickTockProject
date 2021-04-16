13bd93e39b08f65c9a5220c2be00fdf1b83a7bc5
VinayM1010 <72775164+VinayM1010@users.noreply.github.com>
2021-04-15 20:39:50 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,46 @@
+var hr, mn, sc;
+var hra, mna, sca;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);
+  translate(200, 200);  
+  rotate(-90);
+hr = hour();
+mn = minute();
+sc = second();
+sca = map(sc, 0, 60, 0, 360);
+mna = map(mn, 0, 60, 0, 360);
+hra = map(hr%12, 0, 12, 0, 360);
+push();
+rotate(sca);
+stroke("green");
+strokeWeight(7);
+line(0, 0, 100, 0);
+pop();
+push();
+rotate(mna);
+stroke("red");
+strokeWeight(7);
+line(0, 0, 75, 0);
+pop();
+push();
+rotate(hra);
+stroke("yellow");
+strokeWeight(7);
+line(0, 0, 50, 0);
+pop();
+stroke("blue");
+point(0, 0);
+strokeWeight(10);
+arc(0, 0, 300, 300, 0, sca);
+stroke("pink");
+arc(0, 0, 280, 280, 0, mna);
+stroke("white");
+arc(0, 0, 260, 260, 0, hra);
+
   drawSprites();
 }
